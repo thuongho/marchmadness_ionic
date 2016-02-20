204317815c385579daf615c75c17d1e65185f0da
Thuong Ho <thuong.t.ho@gmail.com>
2016-02-19 20:21:11 -0800

    add states and corresponding pages

--- www/app/app.js
+++ www/app/app.js
@@ -21,16 +21,90 @@ angular.module('bballApp', ['ionic'])
   $stateProvider
 
     .state('home', {
+      abstract: true,
       url: '/home',
       templateUrl: 'app/home/home.html'
     })
 
+    .state('home.group', {
+      url: '/group',
+      views: {
+        "tab-group": {
+          templateUrl: "app/home/group.html"
+        }
+      }
+    })
+
+    .state('home.mybracket', {
+      url: '/mybracket',
+      views: {
+        "tab-mybracket": {
+          templateUrl: 'app/home/mybracket.html'
+        }
+      }
+    })
+
     // primary page for the rest of the app
     .state('app', {
+      abstract: true,
       url: '/app',
       templateUrl: 'app/layout/menu-layout.html'
+    })
+
+    .state('app.teams', {
+      url: '/teams',
+      views: {
+        'mainContent': {
+          templateUrl: 'app/teams/teams.html'
+        }
+      }
+    })
+
+    .state('app.team-detail', {
+      url: '/teams/:id',
+      views: {
+        'mainContent': {
+          templateUrl: 'app/teams/team-detail.html'
+        }
+      }
+    })
+
+    .state('app.game', {
+      url: '/game/:id',
+      views: {
+        'mainContent': {
+          templateUrl: 'app/game/game.html'
+        }
+      }
+    })
+
+    .state('app.standings', {
+      url: '/standings',
+      views: {
+        'mainContent': {
+          templateUrl: 'app/standings/standings.html'
+        }
+      }
+    })
+
+    .state('app.locations', {
+      url: '/locations',
+      views: {
+        'mainContent': {
+          templateUrl: 'app/locations/locations.html'
+        }
+      }
+    })
+
+    .state('app.rules', {
+      url: '/rules',
+      views: {
+        'mainContent': {
+          templateUrl: 'app/rules/rules.html'
+        }
+      }
     });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app');
+  $urlRouterProvider.otherwise('/app/teams');
 });
