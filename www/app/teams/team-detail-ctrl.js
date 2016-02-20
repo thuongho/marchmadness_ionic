0607492df22357833d6d6515bb161b3cda2a0717
Thuong Ho <thuong.t.ho@gmail.com>
2016-02-19 20:48:20 -0800

    add team-detail controller

---
+++ www/app/teams/team-detail-ctrl.js
@@ -0,0 +1,7 @@
+(function(){
+  'use strict'
+
+  angular.module('bballApp').controller('teamDetailCtrl', ['$scope', '$stateParams', function ($scope, $stateParams) {
+    console.log($stateParams);
+  }]);
+})();
